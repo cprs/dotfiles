b36c3be582c862a14420a6b1212c698755eb520a
Johan Cuypers <johan.cuypers@engilico.com>
2023-01-30 12:06:29 +0100

    Optimize PowerShell startup

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -12,24 +12,8 @@ function dotfiles {git.exe --git-dir=$home/.dotfiles/ --work-tree=$home $args}
 # Make Tab auto-complete behave more like Bash.
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
-# Alias vi and vim to nvim if it's installed.
-if ((Get-Command nvim -ErrorAction Ignore)) {
-    if (-not (Get-Command vim -ErrorAction Ignore)) {
-        Set-Alias vim nvim
-    }
-    if (-not (Get-Command vi -ErrorAction Ignore)) {
-        Set-Alias vi vim
-    }
-    if (-not (Get-Command gvim -ErrorAction Ignore)) {
-        Set-Alias gvim nvim-qt
-    }
-}
+Set-Alias vim nvim
+Set-Alias vi vim
+Set-Alias gvim nvim-qt
 
-# Import posh-git if it's installed
-If (Get-InstalledModule posh-git -ErrorAction silentlycontinue) {
-  Import-Module posh-git
-}
-Else {
-  Write-Host "posh-git not installed. Install it with:"
-  Write-Host "PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force"
-}
+Import-Module posh-git
