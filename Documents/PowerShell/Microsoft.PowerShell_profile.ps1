ff8b523633e716a1acb4a61c4e60af4724faea98
Johan Cuypers <johan.cuypers@engilico.com>
2023-01-30 12:02:12 +0100

    Aliases for Zoom meetings

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,11 @@
 # Terminal prompt
 oh-my-posh --init --shell pwsh --config 'C:\Program Files (x86)\oh-my-posh\themes\mojada.omp.json' | Invoke-Expression
 
+# Zoom meetings
+function standup {start "zoommtg://zoom.us/join?action=join&confno=200913505&pwd=N1BoUU9LZXZhMnd4dDdCSUl1dGdMdz09"}
+function ryan {start "zoommtg://zoom.us/join?action=join&confno=8052719819"}
+function barry {start "zoommtg://zoom.us/join?action=join&confno=8328088128"}
+
 # Support dotfiles repo
 function dotfiles {git.exe --git-dir=$home/.dotfiles/ --work-tree=$home $args}
 
