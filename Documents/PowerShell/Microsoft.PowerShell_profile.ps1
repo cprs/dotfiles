c233d1a969022d89a9b97abe37b1c0fc59e345bc
Johan Cuypers <johan.cuypers@engilico.com>
2023-04-05 18:16:58 +0200

    More meeting shortcuts

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -5,6 +5,9 @@ oh-my-posh --init --shell pwsh --config 'C:\Program Files (x86)\oh-my-posh\theme
 function standup {start "zoommtg://zoom.us/join?action=join&confno=200913505&pwd=N1BoUU9LZXZhMnd4dDdCSUl1dGdMdz09"}
 function ryan {start "zoommtg://zoom.us/join?action=join&confno=8052719819"}
 function barry {start "zoommtg://zoom.us/join?action=join&confno=8328088128"}
+function demo {start "zoommtg://zoom.us/join?action=join&confno=84294935166&pwd=VzBGMTY3V29ENDFaSThVSkZYNkE1QT09"}
+function project-review {start "zoommtg://zoom.us/join?action=join&confno=87079793189&pwd=ZFBGSm1WMVIzVk52a1ozbjZnZ01lUT09"}
+function cdt {start "zoommtg://zoom.us/join?action=join&confno=96155246494&pwd=RGFyNC9tNzdRN2JOVjlrODg0b201UT09 "}
 
 # Support dotfiles repo
 function dotfiles {git.exe --git-dir=$home/.dotfiles/ --work-tree=$home $args}
